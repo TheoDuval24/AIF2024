6a031030005371d98b7bd069e38129000b712ef5
Duval Theo <tduval@insa-12590>
2023-10-09 15:41:04 +0200

    ajout des modifications

--- developpement/MNIST/mnist_api.py
+++ developpement/MNIST/mnist_api.py
@@ -10,9 +10,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 app = Flask(__name__)
 
-parser = ... 
-... # add an argument '--model_path'
-model_path = ...
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model_path', type=str, default = 'weights/mnist_net.pth', help='weights emplacement')
+args = parser.parse_args()
+model_path = args.model_path
 
 model = MNISTNet().to(device)
 # Load the model
@@ -40,6 +42,26 @@ def predict():
         _, predicted = outputs.max(1)
 
     return jsonify({"prediction": int(predicted[0])})
+
+@app.route('/batch_predict',methods=['POST'])
+def batch_predict():
+    imgs_binary = request.files.getlist("images[]")
+    imgs= []
+    for img_binary in imgs_binary :
+        img_pil = Image.open(img_binary.stream)
+        
+        # Transform the PIL Image
+        tensor = transform(img_pil).to(device)
+        tensor = tensor.unsqueeze(0)
+        imgs.append(tensor)
+    batch = torch.cat(imgs, dim = 0)
+    
+    # Make prediction
+    with torch.no_grad():
+        output = model(batch)
+        _, pred = output.max(1)
+        
+    return jsonify({"predictions" : pred.tolist()})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
